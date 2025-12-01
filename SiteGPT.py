437fa2b14f817d295d5677c0bd605ae9ea469ee8
halfmin <halfmin@gmail.com>
2025-12-01 13:21:40 +0900

    10.4

--- SiteGPT.py
+++ SiteGPT.py
@@ -1,7 +1,10 @@
-from langchain.document_loaders import AsyncChromiumLoader, SitemapLoader
-from langchain.document_transformers import Html2TextTransformer
-from fake_useragent import UserAgent
+from langchain.document_loaders import SitemapLoader
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 import streamlit as st
 import sys
 import asyncio
@@ -13,13 +16,50 @@ if sys.platform.startswith("win"):
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
 
-# Initialize a UserAgent object
-ua = UserAgent()
-
-st.set_page_config(
-    page_title="SiteGPT",
-    page_icon="🖥️",
+llm = ChatOpenAI(
+    temperature=0.1,
 )
+
+answers_prompt = ChatPromptTemplate.from_template(
+    """
+    Using ONLY the following context answer the user's question. If you can't just say you don't know, don't make anything up.
+                                                  
+    Then, give a score to the answer between 0 and 5.
+
+    If the answer answers the user question the score should be high, else it should be low.
+
+    Make sure to always include the answer's score even if it's 0.
+
+    Context: {context}
+                                                  
+    Examples:
+                                                  
+    Question: How far away is the moon?
+    Answer: The moon is 384,400 km away.
+    Score: 5
+                                                  
+    Question: How far away is the sun?
+    Answer: I don't know
+    Score: 0
+                                                  
+    Your turn!
+
+    Question: {question}
+"""
+)
+
+
+def get_answers(inputs):
+    docs = inputs["docs"]
+    question = inputs["question"]
+    answers_chain = answers_prompt | llm
+    answers = []
+    for doc in docs:
+        result = answers_chain.invoke(
+            {"question": question, "context": doc.page_content}
+        )
+        answers.append(result.content)
+    st.write(answers)
 
 
 def parse_page(soup):
@@ -39,31 +79,25 @@ def parse_page(soup):
 
 @st.cache_data(show_spinner="Loading website...")
 def load_website(url):
-    try:
-        splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-            chunk_size=1000,
-            chunk_overlap=200,
-        )
-        loader = SitemapLoader(
-            url,
-            filter_urls=[
-                r"^(.*\/blog\/).*",
-            ],
-            parsing_function=parse_page,
-        )
-        loader.requests_per_second = 2
-        loader.requests_kwargs = {"verify": False}
-        # Set a realistic user agent
-        loader.headers = {'User-Agent': ua.random}
-        #docs = loader.load()
-        docs = loader.load_and_split(text_splitter=splitter)
-        logging.debug(f"Loaded documents: {docs}")
-        return docs
-    except Exception as e:
-        logging.error(f"Error loading sitemap: {e}")
-        return []
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    loader = SitemapLoader(
+        url,
+        parsing_function=parse_page,
+    )
+    loader.requests_per_second = 2
+    docs = loader.load_and_split(text_splitter=splitter)
+    vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
+    return vector_store.as_retriever()
 
-html2text_transformer = Html2TextTransformer()
+
+st.set_page_config(
+    page_title="SiteGPT",
+    page_icon="🖥️",
+)
+
 
 st.markdown(
     """
@@ -75,12 +109,12 @@ st.markdown(
 """
 )
 
+
 with st.sidebar:
     url = st.text_input(
         "Write down a URL",
         placeholder="https://example.com",
     )
-    st.write("https://www.gamsgo.com/sitemap_index.xml")
 
 
 if url:
@@ -88,14 +122,11 @@ if url:
         with st.sidebar:
             st.error("Please write down a Sitemap URL.")
     else:
-        docs = load_website(url.strip())
-        if docs:
-            st.write(docs)
-        else:
-            st.error(
-                "Failed to load documents from the sitemap. Please check the URL and try again."
-            )
-    # loader = AsyncChromiumLoader([url])
-    # docs = loader.load()
-    # transformed = html2text_transformer.transform_documents(docs)
-    # st.write(docs)
+        retriever = load_website(url)
+
+        chain = {
+            "docs": retriever,
+            "question": RunnablePassthrough(),
+        } | RunnableLambda(get_answers)
+
+        chain.invoke("What is the pricing of GPT-4 Turbo with vision.")
